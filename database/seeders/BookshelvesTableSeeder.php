61142e8795801ccf5580006e55e2afa27d1d4541
alm191 <alm191@alu.ua.es>
2023-03-01 21:11:28 +0100

    añadidas factoria y seeder de suggestions

--- database/seeders/BookshelvesTableSeeder.php
+++ database/seeders/BookshelvesTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Bookshelf;
 
 class BookshelvesTableSeeder extends Seeder
 {
@@ -14,6 +15,7 @@ class BookshelvesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //use factory
+        // Bookshelf::factory()->count(10)->create();
     }
 }
