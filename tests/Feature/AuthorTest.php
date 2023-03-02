5b6bfdfa8e637b412d25a6cdd8a27abbbfcb5f69
Ilyas Bsadou <ibb30@alu.ua.es>
2023-03-02 15:48:53 +0100

    Primer Test de Author

--- tests/Feature/AuthorTest.php
+++ tests/Feature/AuthorTest.php
@@ -4,9 +4,13 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
+
 
 use Tests\TestCase;
 use App\Models\Author;
+use App\Models\Book;
 
 class AuthorTest extends TestCase
 {
@@ -15,8 +19,28 @@ class AuthorTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+
+    // Funcion Basica de prueba de la Clase Autor 
+    public function test_example_author()
     {
-        $this->assertTrue(true);
+        $texto = "J​eff Kinney, es el creador de la serie El diario de Greg. También creó la página web Poptropica.​";
+
+        $autor = new Author();
+        $autor->id = 1 ; 
+        $autor->name = "Jeff Kinney​​" ;
+        $autor->birthdate = "​19/2/1971​" ;
+        $autor->isDeath = false ;
+        $autor->deathdate = "-1​" ;
+        $autor->info= $texto ;
+
+        // Comparaciones
+        $this->assertEquals(1,$autor->id);
+        $this->assertEquals("​19/2/1971​",$autor->birthdate);
+        $this->assertFalse($autor->isDeath);
+        $this->assertEquals("-1",$autor->deathdate);
+        $this->assertEquals($texto,$autor->info);
     }
+
 }
+
+///vendor/bin/phpunit --filter AuthorTest
