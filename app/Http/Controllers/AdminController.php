3c7b357f0751efe7a08e5c2b61e8890ad8e534bb
alm191 <alm191@alu.ua.es>
2023-03-22 09:21:43 +0100

    algo

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -13,33 +13,35 @@ class AdminController extends Controller
 {
     public function index()
     {
+        $PER_PAGE = 5;
+
         $books = Book::paginate(
-            $perPage = 5,
+            $perPage = $PER_PAGE,
             // all columns except created_at, updated_at, deleted_at, isbn
             $columns = ['id', 'title', 'description', 'author_id', 'category_id', 'image'],
             $pageName = 'books',
         );
         $authors = Author::paginate(
-            $perPage = 5,
+            $perPage = $PER_PAGE,
             // all columns except created_at, updated_at, deleted_at
             $columns = ['id', 'name', 'info', ], //'image'
             $pageName = 'authors',
         );
         $users = User::paginate(
-            $perPage = 5,
+            $perPage = $PER_PAGE,
             // all columns except created_at, updated_at, deleted_at
             $columns = ['id', 'username', 'email', 'is_admin', ], //'image'
             $pageName = 'users',
         );
         $categories = Category::orderBy('id', 'asc')->
         paginate(
-            $perPage = 5,
+            $perPage = $PER_PAGE,
             // all columns except created_at, updated_at, deleted_at
             $columns = ['id', 'tag', ], 
             $pageName = 'categories',
         );
         $suggestions = Suggestion::paginate(
-            $perPage = 5,
+            $perPage = $PER_PAGE,
             // all columns except created_at, updated_at, deleted_at
             $columns = ['id', 'email', 'phone', 'message', ],
             $pageName = 'suggestions',
