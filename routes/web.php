d0693de9dd71e68d5c8dfeac8552d04398adc041
alm191 <alm191@alu.ua.es>
2023-05-09 12:42:06 +0200

    cambiado formato de rutas

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,9 @@
 
 // use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
-// use App\Http\Controllers\Auth;
+// use Controller\Auth;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers as Controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,7 +23,7 @@ Route::group(['middleware' => 'language'], function () {
         return view('forms.example');
     })->name('example-form');
     
-    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/', [Controller\HomeController::class, 'index'])->name('home');
     Route::get('/404', function () {
         return view('errors.404');
     })->name('404');
@@ -30,84 +31,84 @@ Route::group(['middleware' => 'language'], function () {
         return view('terms');
     })->name('terms');
     
-    Route::post('/newsletter', [App\Http\Controllers\HomeController::class, 'newsletterStore'])->name('newsletter.store');
+    Route::post('/newsletter', [Controller\HomeController::class, 'newsletterStore'])->name('newsletter.store');
 
     Route::group(['middleware' => ['guest']], function() {
-        Route::get('/login', [App\Http\Controllers\LoginController::class, 'showLogin'])->name('login');
-        Route::post('/login', [App\Http\Controllers\LoginController::class, 'login'])->name('login');
-        // Route::get('/register', [App\Http\Controllers\RegisterController::class, 'showRegister'])->name('register.show');
-        // Route::post('/register', [App\Http\Controllers\RegisterController::class, 'register'])->name('register.perform');
+        Route::get('/login', [Controller\LoginController::class, 'showLogin'])->name('login');
+        Route::post('/login', [Controller\LoginController::class, 'login'])->name('login');
+        // Route::get('/register', [Controller\RegisterController::class, 'showRegister'])->name('register.show');
+        // Route::post('/register', [Controller\RegisterController::class, 'register'])->name('register.perform');
     });
     
     Route::group(['middleware' => ['auth']], function() {
-        Route::get('/logout', [App\Http\Controllers\LogoutController::class, 'logout'])->name('logout');
+        Route::get('/logout', [Controller\LogoutController::class, 'logout'])->name('logout');
         
         Route::prefix('book')->group(function () {
-            Route::get('/{id}', [App\Http\Controllers\BooksController::class, 'index', 'id'])->name('book');
-            Route::get('/download/{id}', [App\Http\Controllers\BooksController::class, 'download', 'id'])->name('book-download');
-            Route::get('/read/{id}', [App\Http\Controllers\BooksController::class, 'showFile', 'id'])->name('book-read');
+            Route::get('/{id}', [Controller\BooksController::class, 'index', 'id'])->name('book');
+            Route::get('/download/{id}', [Controller\BooksController::class, 'download', 'id'])->name('book-download');
+            Route::get('/read/{id}', [Controller\BooksController::class, 'showFile', 'id'])->name('book-read');
                 
         });
 
         Route::prefix('bookshelf')->group(function () {
-            Route::post('/create', [App\Http\Controllers\BookshelfController::class, 'store'])->name('bookshelf.store');
-            Route::put('/add/{book_id}', [App\Http\Controllers\BookshelfController::class, 'add_book', 'book_id'])->name('bookshelf.add_book');
-            Route::get('/list', [App\Http\Controllers\BookshelfController::class, 'list'])->name('bookshelf.list');
-            Route::delete('/delete/{id}', [App\Http\Controllers\BookshelfController::class, 'delete', 'id'])->name('bookshelf.delete');
-            Route::get('/remove/{bookshelf_id}/{book_id}', [App\Http\Controllers\BookshelfController::class, 'remove_book', 'bookshelf_id', 'book_id'])->name('bookshelf.remove_book');
+            Route::post('/create', [Controller\BookshelfController::class, 'store'])->name('bookshelf.store');
+            Route::put('/add/{book_id}', [Controller\BookshelfController::class, 'add_book', 'book_id'])->name('bookshelf.add_book');
+            Route::get('/list', [Controller\BookshelfController::class, 'list'])->name('bookshelf.list');
+            Route::delete('/delete/{id}', [Controller\BookshelfController::class, 'delete', 'id'])->name('bookshelf.delete');
+            Route::get('/remove/{bookshelf_id}/{book_id}', [Controller\BookshelfController::class, 'remove_book', 'bookshelf_id', 'book_id'])->name('bookshelf.remove_book');
 
         });
 
-        Route::put('/review/{id}/create', [App\Http\Controllers\ReviewsController::class, 'store', 'id'])->name('review.store');
+        Route::put('/review/{id}/create', [Controller\ReviewsController::class, 'store', 'id'])->name('review.store');
     
-        // Route::delete('/user/softdelete/{id}', [App\Http\Controllers\UsersController::class, 'softDelete', 'id'])->name('user.softdelete');
+        // Route::delete('/user/softdelete/{id}', [Controller\UsersController::class, 'softDelete', 'id'])->name('user.softdelete');
     });
     
     Route::group(['middleware' => ['admin']], function() {
-        Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->name('admin');
+        Route::get('/admin', [Controller\AdminController::class, 'index'])->name('admin');
         
-        Route::get('/books/create', [App\Http\Controllers\BooksController::class, 'create'])->name('book.create');
-        Route::post('/books/create', [App\Http\Controllers\BooksController::class, 'store'])->name('book.store');
-        Route::get('/books/delete/{id}', [App\Http\Controllers\BooksController::class, 'delete', 'id'])->name('book.delete');
-        Route::put('/books/update/{id}', [App\Http\Controllers\BooksController::class, 'update', 'id'])->name('book.edit');
+        Route::get('/books/create', [Controller\BooksController::class, 'create'])->name('book.create');
+        Route::post('/books/create', [Controller\BooksController::class, 'store'])->name('book.store');
+        Route::get('/books/delete/{id}', [Controller\BooksController::class, 'delete', 'id'])->name('book.delete');
+        Route::put('/books/update/{id}', [Controller\BooksController::class, 'update', 'id'])->name('book.edit');
         
-        Route::get('/author/create', [App\Http\Controllers\AuthorsController::class, 'create'])->name('author.create');
-        Route::post('/author/create', [App\Http\Controllers\AuthorsController::class, 'store'])->name('author.store');
-        Route::put('/author/update/{id}', [App\Http\Controllers\AuthorsController::class, 'update', 'id'])->name('author.edit');
-        Route::get('/author/delete/{id}', [App\Http\Controllers\AuthorsController::class, 'delete', 'id'])->name('author.delete');
+        Route::get('/author/create', [Controller\AuthorsController::class, 'create'])->name('author.create');
+        Route::post('/author/create', [Controller\AuthorsController::class, 'store'])->name('author.store');
+        Route::put('/author/update/{id}', [Controller\AuthorsController::class, 'update', 'id'])->name('author.edit');
+        Route::get('/author/delete/{id}', [Controller\AuthorsController::class, 'delete', 'id'])->name('author.delete');
         
-        Route::post('/category/create', [App\Http\Controllers\CategoriesController::class, 'store'])->name('category.store');
-        Route::put('/category/update/{id}', [App\Http\Controllers\CategoriesController::class, 'update', 'id'])->name('category.edit');
-        Route::get('/category/delete/{id}', [App\Http\Controllers\CategoriesController::class, 'delete', 'id'])->name('category.delete');
+        Route::post('/category/create', [Controller\CategoriesController::class, 'store'])->name('category.store');
+        Route::put('/category/update/{id}', [Controller\CategoriesController::class, 'update', 'id'])->name('category.edit');
+        Route::get('/category/delete/{id}', [Controller\CategoriesController::class, 'delete', 'id'])->name('category.delete');
         
         Route::get('/user/delete/{id}', [App\Services\UsersServices::class, 'delete', 'id'])->name('user.delete');
     
-        Route::post('/send_newsletter', [App\Http\Controllers\MailController::class, 'send_newsletter'])->name('newsletter.send');
+        Route::post('/send_newsletter', [Controller\MailController::class, 'send_newsletter'])->name('newsletter.send');
     });
     
-    Route::get('/profile', [App\Http\Controllers\UsersController::class, 'showProfile'])->name('profile');
+    Route::get('/profile', [Controller\UsersController::class, 'showProfile'])->name('profile');
     
 
-    Route::get('/user/edit/{id}', [App\Http\Controllers\UsersController::class, 'showEdit', 'id'])->name('user-edit.show');
+    Route::get('/user/edit/{id}', [Controller\UsersController::class, 'showEdit', 'id'])->name('user-edit.show');
     
-    Route::get('/books', [App\Http\Controllers\BooksController::class, 'list'])->name('books-list');
+    Route::get('/books', [Controller\BooksController::class, 'list'])->name('books-list');
     
-    Route::get('/author/{id}', [App\Http\Controllers\AuthorsController::class, 'show', 'id'])->name('author');
-    Route::get('/authors', [App\Http\Controllers\AuthorsController::class, 'index'])->name('authors');
+    Route::get('/author/{id}', [Controller\AuthorsController::class, 'show', 'id'])->name('author');
+    Route::get('/authors', [Controller\AuthorsController::class, 'index'])->name('authors');
     
-    Route::put('/suggestion/create', [App\Http\Controllers\SuggestionsController::class, 'store'])->name('suggestion.store');
-    Route::put('/suggestion/update/{id}', [App\Http\Controllers\SuggestionsController::class, 'update', 'id'])->name('suggestion.edit');
-    Route::get('/suggestion/delete/{id}', [App\Http\Controllers\SuggestionsController::class, 'delete', 'id'])->name('suggestion.delete');
+    Route::put('/suggestion/create', [Controller\SuggestionsController::class, 'store'])->name('suggestion.store');
+    Route::put('/suggestion/update/{id}', [Controller\SuggestionsController::class, 'update', 'id'])->name('suggestion.edit');
+    Route::get('/suggestion/delete/{id}', [Controller\SuggestionsController::class, 'delete', 'id'])->name('suggestion.delete');
     
-    Route::put('/user/update/{id}', [App\Http\Controllers\UsersController::class, 'update', 'id'])->name('user.edit');
+    Route::put('/user/update/{id}', [Controller\UsersController::class, 'update', 'id'])->name('user.edit');
 
 
-    Route::put('/review/update/{id}', [App\Http\Controllers\ReviewsController::class, 'update', 'id'])->name('review.edit');
-    Route::get('/review/delete/{id}', [App\Http\Controllers\ReviewsController::class, 'delete', 'id'])->name('review.delete');
+    Route::put('/review/update/{id}', [Controller\ReviewsController::class, 'update', 'id'])->name('review.edit');
+    Route::get('/review/delete/{id}', [Controller\ReviewsController::class, 'delete', 'id'])->name('review.delete');
     
-    Route::get('/contactus', [App\Http\Controllers\ContactusController::class, 'index'])->name('contactus');
-    Route::get('/aboutus', [App\Http\Controllers\AboutusController::class, 'index'])->name('aboutus');
-    // Route::post('/contactus', [App\Http\Controllers\ContactusController::class, 'store'])->name('contactus.store');
+    Route::get('/contactus', [Controller\ContactusController::class, 'index'])->name('contactus');
+    Route::get('/aboutus', [Controller\AboutusController::class, 'index'])->name('aboutus');
+    // Route::post('/contactus', [Controller\ContactusController::class, 'store'])->name('contactus.store');
     
     
     Route::get('/lang/{locale}', function ($locale) {
@@ -120,6 +121,6 @@ Route::group(['middleware' => 'language'], function () {
 });
 Auth::routes(['login' => false]);
 
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [Controller\HomeController::class, 'index'])->name('home');
 
 
