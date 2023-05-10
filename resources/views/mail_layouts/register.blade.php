bf23a25b0c72593b869430f26e3ac34120a22154
Alejandro Molines Molina <amm575@clLTV2-3>
2023-05-10 10:24:29 +0200

    Comienzo pag web de email

--- resources/views/mail_layouts/register.blade.php
+++ resources/views/mail_layouts/register.blade.php
@@ -1,7 +1,75 @@
-{{-- Thanks for register email template --}}
-<h1>Hi {{ $name }},</h1>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registrado en WordWaves</title>
+    <!-- <link rel="stylesheet" href="{{ asset('css/mail.css') }}"> -->
+    <!-- No se si poner lo del css, porque al no poder comprobarlo prefiero no mezclar cosas-->
+	<style type="text/css">
+		.container{
+			max-width: 600px;
+			margin: auto;
+			font-family: Arial, Helvetica, sans-serif;
+			background-color: #F4F6FA;
+			padding: 20px;
+			border-radius: 10px;
+		}
 
-<p>Thank you for registering with us.</p>
+		h1{
+			color: #1265C8;
+			text-align: center;
+			font-size: 32px;
+			margin-bottom: 20px;
+		}
 
-<p>Thanks,<br>
-Word Waves :D</p>
+		p{
+			color: #666;
+			font-size: 18px;
+			line-height: 1.5;
+			margin-bottom: 20px;
+			text-align: justify;
+		}
+
+		.button{
+			display: block;
+			background-color: #1265C8;
+			color: #fff;
+			padding: 10px 20px;
+			border-radius: 5px;
+			text-align: center;
+			text-decoration: none;
+			font-size: 20px;
+			margin: 20px auto;
+			max-width: 200px;
+		}
+
+		.button:hover{
+			background-color: #0B3E7B;
+		}
+
+		img{
+			max-width: 100%;
+			height: auto;
+			margin-bottom: 20px;
+		}
+	</style>
+</head>
+<body>
+	<div class="container">
+		<img src="{{asset ('images/logo.png')}}" alt="Logo de WordWaves">
+        <h1>Hi {{ $name }},</h1>
+        <h1 style="border-bottom: 2px solid #ADD8E6; padding-bottom: 2px;"></h1>
+
+        <p>Thank you for registering with us.</p>
+        <p>Thanks,<br>
+        Word Waves :D</p>
+		<a class="button" href="#">Visit our web</a>
+	</div>
+</body>
+</html>
+
+
+
+
+
+
+
