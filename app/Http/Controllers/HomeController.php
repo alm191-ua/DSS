8e7f6154f90134a3199c6b2a2c1baf239652f7da
Salily31 <ibb30@alu.ua.es>
2023-05-17 00:02:45 +0200

    Actualizacion del Index

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,16 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Book;
+use App\Models\Author;
 
 class HomeController extends Controller
 {
     //
     public function index(){
+        $titulos= array("NUEVOS PRODUCTOS" , "BEST SELLER" , "PRE-ORDER");
         $top_books = Book::inRandomOrder()->take(4)->get();
+        $new_books = Book::inRandomOrder()->take(12)->get();
+        $new_authors = Author::inRandomOrder()->take(3)->get();
         $num_authors = Book::distinct('author_id')->count('author_id');
         $num_books = Book::count();
 
-        return view('welcome', compact('top_books', 'num_authors', 'num_books'));
+        return view('welcome', compact('top_books', 'num_authors', 'num_books','new_books','titulos','new_authors'));
     }
 
     public function newsletterStore(Request $request){
